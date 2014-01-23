85e3919b900796138f7d4045c5c2d9d3c4e398e6
Nina Stolze <nina.stolze@ffcg.se>
2014-01-23 20:14:32 +0100

    Added default command processor

---
+++ Argentum.Core/DefaultCommandProcessor.cs
@@ -0,0 +1,22 @@
+﻿using TinyIoC;
+
+namespace Argentum.Core
+{
+	public class DefaultCommandProcessor : IProcessCommand
+	{
+		private readonly TinyIoCContainer container;
+
+		public DefaultCommandProcessor(TinyIoCContainer container)
+		{
+			this.container = container;
+		}
+
+		public void Process(ICommand command)
+		{
+			var handlerType = typeof (IHandleCommand<>).MakeGenericType(command.GetType());
+			dynamic handler = container.Resolve(handlerType);
+
+			handler.HandleCommand((dynamic) command);
+		}
+	}
+}
