ae443ddb1d6e7623adc57e70ccc8f1d7fd54e5b4
Jens Pettersson <jens.pettersson@live.se>
2014-01-23 17:17:57 +0100

    Added basic project structure

---
+++ Argentum.Core.Tests/SomeTestClass.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Argentum.Core.Tests
+{    
+    public class SomeTestClass : IUseFixture<TestSetupFixture>
+    {
+        [Fact]
+        public void SomeTest()
+        {
+            
+        }
+
+        public void SetFixture(TestSetupFixture data)
+        {
+            data.DoStuff();
+        }
+    }
+
+    public class TestSetupFixture
+    {
+        public void DoStuff()
+        {
+            
+        }
+    }
+}
