387d5eac5b00a1f216ad18d3d25dfea659c162f8
Nina <nina.stolze@ffcg.se>
2014-05-07 20:16:16 +0200

    Can create bookable show

--- Sample/SilverScreen/Domain/BookableShow.cs
+++ Sample/SilverScreen/Domain/BookableShow.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using Argentum.Core;
 
 namespace SilverScreen.Domain
 {
 	public class BookableShow : AggregateBase<BookableShowState>
 	{
 		public BookableShow(BookableShowState state) : base(state) { }
+
+		private BookableShow(Guid id, Guid screenId, DateTime showTime, List<BookableSeat> seats)
+		{
+			Apply(new BookableShowCreated(id, screenId, showTime, seats));
+		}
+
+		public static BookableShow Create(Guid id, Guid screenId, DateTime showTime, List<BookableSeat> seats)
+		{
+			return new BookableShow(Guid.NewGuid(), screenId, showTime, seats);
+		}
 	}
 
 	public class BookableShowState : State
@@ -14,6 +25,30 @@ namespace SilverScreen.Domain
 
 		public DateTime ShowTime { get; set; }
 
-		public List<BookableSeat> Seats { get; set; } 
+		public List<BookableSeat> Seats { get; set; }
+
+		public void When(BookableShowCreated evt)
+		{
+			Id = evt.Id;
+			ScreenId = evt.ScreenId;
+			ShowTime = evt.ShowTime;
+			Seats = evt.Seats;
+		}
+	}
+
+	public class BookableShowCreated : IEvent
+	{
+		public Guid Id { get; private set; }
+		public Guid ScreenId { get; private set; }
+		public DateTime ShowTime { get; private set; }
+		public List<BookableSeat> Seats{ get; private set; }
+
+		public BookableShowCreated(Guid id, Guid screenId, DateTime showTime, List<BookableSeat> seats)
+		{
+			Id = id;
+			ScreenId = screenId;
+			ShowTime = showTime;
+			Seats = seats;
+		}
 	}
 }
