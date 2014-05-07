c9972b75084eb0afab550545827974985399a993
Nina <nina.stolze@ffcg.se>
2014-05-07 20:05:57 +0200

    Ability to create a new booking

--- Sample/SilverScreen/Domain/Booking.cs
+++ Sample/SilverScreen/Domain/Booking.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using Argentum.Core;
 
 namespace SilverScreen.Domain
 {
 	public class Booking : AggregateBase<BookingState>
 	{
 		public Booking(BookingState state) : base(state) { }
+
+		private Booking(Guid id, Guid bookableShowId, List<Seat> bookedSeats, DateTime created)
+		{
+			Apply(new BookingCreated(id, bookableShowId, bookedSeats, created));
+		}
+
+		public static Booking Create(Guid bookableShowId, List<Seat> bookedSeats)
+		{
+			return new Booking(Guid.NewGuid(), bookableShowId, bookedSeats, DateTime.UtcNow);
+		}
 	}
 
 	public class BookingState : State
@@ -13,5 +24,31 @@ namespace SilverScreen.Domain
 		public Guid BookableShowId { get; set; }
 
 		public List<Seat> BookedSeats { get; set; }
+		
+		public DateTime Created { get; set; }
+
+		public void When(BookingCreated evt)
+		{
+			Id = evt.Id;
+			BookableShowId = evt.BookableShowId;
+			BookedSeats = evt.BookedSeats;
+			Created = evt.Created;
+		}
+	}
+
+	public class BookingCreated : IEvent
+	{
+		public Guid Id { get; private set; }
+		public Guid BookableShowId { get; private set; }
+		public List<Seat> BookedSeats { get; private set; }
+		public DateTime Created { get; private set; }
+
+		public BookingCreated(Guid id, Guid bookableShowId, List<Seat> bookedSeats, DateTime created)
+		{
+			Id = id;
+			BookableShowId = bookableShowId;
+			BookedSeats = bookedSeats;
+			Created = created;
+		}
 	}
 }
