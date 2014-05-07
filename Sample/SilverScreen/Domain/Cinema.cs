346e21cf3f44dbb976e1a31079ac45606da9bdd7
Nina <nina.stolze@ffcg.se>
2014-05-07 19:29:43 +0200

    Added event for creating cinema

--- Sample/SilverScreen/Domain/Cinema.cs
+++ Sample/SilverScreen/Domain/Cinema.cs
@@ -1,14 +1,42 @@
 ﻿using System.Collections.Generic;
+using Argentum.Core;
 
 namespace SilverScreen.Domain
 {
 	public class Cinema : AggregateBase<CinemaState>
 	{
 		public Cinema(CinemaState state) : base(state) { }
+
+		private Cinema(string name)
+        {
+            Apply(new CinemaCreated(name));
+        }
+
+		public static Cinema Create(string name, string adress)
+        {
+			return new Cinema(name);
+        }
 	}
 
 	public class CinemaState : State
 	{
+		public string Name { get; set; }
+
 		public List<Screen> Screens { get; set; }
+
+		public void When(CinemaCreated evt)
+		{
+			Name = evt.Name;
+		}
+	}
+
+	public class CinemaCreated : IEvent
+	{
+		public string Name { get; private set; }
+
+		public CinemaCreated(string name)
+		{
+			Name = name;
+		}
 	}
 }
